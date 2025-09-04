dbd83edb82f47b566cfabd352b5695c5c8024bd4
mahatabul <rozinislam2002@gmail.com>
2025-09-04 17:40:44 +0600

    updated todo list

--- to-dolist/script.js
+++ to-dolist/script.js
@@ -16,13 +16,26 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
 tasklist.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 },false);
+
+function saveData(){
+    localStorage.setItem("task",tasklist.innerHTML);
+
+};
+
+function showTask(){
+    tasklist.innerHTML = localStorage.getItem("task");
+};
+showTask();
